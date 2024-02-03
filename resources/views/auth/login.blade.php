22396175e65a3f1391bfa997b64b37cff76c6783
hiromu <c0b2209617@edu.teu.ac.jp>
2024-02-03 20:53:13 +0900

    ログイン画面の完成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,19 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/style.css">
 </head>
 <body>
-    <h1>ログイン</h1>
-    <form action="{{ route('login') }}" method="post">
-        @csrf 
-        <dl class="form-list">
-            <dt>メールアドレス</dt>
-            <dd><input type="email" name="email" value="{{ old('email') }}"></dd>
-            <dt>パスワード</dt>
-            <dd><input type="password" name="password"></dd>
-        </dl>
-        <button type="submit">ログイン</button>
-        <a href="/">キャンセル</a>
-    </form>
+    <div class="container">
+        <h1>ログイン</h1>
+        <form action="{{ route('login') }}" method="post">
+            @csrf 
+            <div class="form-item">
+                <input type="email" name="email"  placeholder="Email Address" value="{{ old('email') }}"></input>
+            </div>
+            <div class="form-item">
+                <input type="password" name="password" placeholder="Password"></input>
+            </div>
+            <div class="button-panel">
+                <input type="submit" title="Login" value="Login"></input>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
